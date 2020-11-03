2b7b1f752d01e006ab4ccf4a2c7d1a3ee204fdbf
Tirdys <55100702+Tirdys@users.noreply.github.com>
2020-11-03 17:43:37 +0100

    adding_code

---
+++ fewd_lab_7.js
@@ -0,0 +1,48 @@
+var firstName = "Antoine";
+var favouriteColor = "Green";
+var favouriteFood = "Lasagne";
+var city = "Paris";
+
+// This code display who i am and what i like :)
+console.log(
+  "My first name is " +
+    firstName +
+    "and my favourite color is " +
+    favouriteColor +
+    "and my favourite food is " +
+    favouriteFood
+);
+
+var phrase =
+  "A rainbow is formed by the sunlight. Shining through rain drops so small. The rain bends the light from the sunshine, and colors the sky for us all.";
+var endingPhrase =
+  "is what we see. Rainbow colours, are made with light bending you see!";
+var one = "red";
+var two = "orange";
+var three = "yellow";
+var four = "green";
+var five = "blue";
+var six = "indigo";
+var seven = "violet";
+
+console.log(
+  phrase +
+    one +
+    ", " +
+    two +
+    ", " +
+    three +
+    ", " +
+    four +
+    ", " +
+    five +
+    ", " +
+    six +
+    ", " +
+    seven +
+    endingPhrase
+);
+
+var myAlphabet = ["A", "B", "C", "D", "E", "F", "G"];
+
+console.log(myAlphabet.length);
